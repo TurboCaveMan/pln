743622a8f308d01309c1a48eb0d89a9aa6e41b0d
TurboCaveMan <cameronturner55@gmail.com>
2022-09-18 18:53:32 -0700

    Create Project.sol

---
+++ Project.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Project {
+
+
+    struct User_Project{
+        address Creator;
+        string Title;
+        string Project_Type;
+        string Location;
+        string Description;
+    }
+
+    User_Project[] User_Projects;
+
+    function create_project(User_Project calldata _Project_Info) external {
+        User_Project memory New_Project;
+
+        New_Project = _Project_Info;
+        User_Projects.push(New_Project);
+    }
+         
+}
