9c65e89f101dabd43e31e82a1c5c3a9177d3d1f2
TurboCaveMan <cameronturner55@gmail.com>
2022-10-18 00:45:01 -0700

    Create Transfer.sol
    still not working

---
+++ Transfer.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+//import "./PLN_Token.sol";
+import "./EscrowContract.sol";
+
+contract Transfer {
+
+    address payable public admin;
+    address payable public vault;
+    uint public process_fee;
+    uint public amount_to_bidder;
+    uint public amount_to_vault;
+
+    address public PLN_address;
+    uint public amount_to_mint;
+    uint public total_supply;
+
+    constructor() {
+        admin = payable(msg.sender);
+        vault = payable(msg.sender);
+        // amount_to_mint = 10;
+        process_fee = 975;
+        // amount_to_bidder = 8;
+        // amount_to_vault = 2;
+        // total_supply = 100000000 ether;
+
+    }
+
+    // // Token Settings
+
+    // function setPLNaddress(address _address_PLN) external{
+    //     require(msg.sender == admin);
+    //     PLN_address = _address_PLN;
+    // }
+
+    // function callmint(address _toAddress, uint amount) private {
+    //     Plnning Token = Plnning(PLN_address);
+    //     Token.mint(_toAddress, amount);
+    // }
+
+    // function setamount_to_bidder(uint _amount) external{
+    //     require(msg.sender == admin);
+    //     require(_amount < 10);
+    //     amount_to_bidder = _amount;
+    //     amount_to_vault = 10 - _amount;
+    // }
+
+    // function setamount_to_vault(uint _amount) external{
+    //     require(msg.sender == admin);
+    //     require(_amount < 10);
+    //     amount_to_vault = _amount;
+    //     amount_to_bidder = 10 - _amount;
+    // }
+
+
+    // Pay_User Settings
+
+    struct User{
+        address payable UserAddress;
+        uint Bidfee; 
+        address payable arbitrator;
+    }    
+
+    function setProcessFee(uint _NewProcessFee) external{
+        require(msg.sender == admin);
+        require(_NewProcessFee < 1000);
+        process_fee = _NewProcessFee;
+    }
+
+    function setAdmin(address payable _NewAdmin) external{
+        require(msg.sender == admin);
+        admin = _NewAdmin;
+    }
+
+    function setvault(address payable _NewVault) external{
+        require(msg.sender == admin);
+        vault = _NewVault;
+    }
+
+    // Functions
+    function Create_Escrow (address payable Paying, address payable Payee, address payable arbitrator) returns(payable address) {
+        EscrowContract escrow = new EscrowContract(Paying,  Payee, arbitrator);
+        return address(escrow);
+    } 
+
+
+    function Pay_Users(User[] calldata _List) external payable {
+        require(msg.sender != admin);
+        require(msg.sender != vault);
+
+        uint _Bidderfee;
+        uint _totalbidderfee = 0;
+        uint _totalpaid = 0;
+        address payable Escrow_Contract_Address;
+
+        User memory Payee;
+        
+        for(uint i = 0; i < _List.length; i++) {
+            Payee = _List[i];
+
+            _totalpaid = _totalpaid + Payee.Bidfee;            
+            _Bidderfee = Payee.Bidfee * process_fee / 1000;
+            _totalbidderfee = _totalbidderfee + _Bidderfee; 
+
+            Escrow_Contract_Address = Create_Escrow(msg.sender, Payee.UserAddress, Payee.arbitrator);
+
+            Escrow_Contract_Address.transfer(_Bidderfee);
+//            callmint(Payee.UserAddress, amount_to_mint*Payee.Bidfee*amount_to_bidder);
+
+//            total_supply = total_supply + amount_to_mint*Payee.Bidfee*amount_to_bidder;
+
+        }
+
+        if(msg.value < _totalpaid) {
+            revert();
+        }
+
+        vault.transfer(_totalpaid-_totalbidderfee);
+//        callmint(vault, amount_to_mint*_totalpaid*amount_to_vault);
+        
+//        total_supply = total_supply + amount_to_mint*_totalpaid*amount_to_vault;
+
+//        amount_to_mint = total_supply / 10000000 ether;
+
+    }
+}
